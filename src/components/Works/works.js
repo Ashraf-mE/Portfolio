e9ecdc78778ede5eba8840eed3606a67d08975fa
Ashraf-mE <mohammadashrafp10@gmail.com>
2024-08-25 15:20:06 +0530

    images projects

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -10,7 +10,7 @@ const Works = () => {
         quia et omnis excepturi accusantium ducimus aut? Molestias, est nisi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto beatae dolore voluptates deleniti, vitae dolores nulla vel aliquam perspiciatis quam quas praesentium exercitationem. Sit minima totam id ipsa repudiandae laboriosam.
       </span>
       <div className="worksImgs">
-        <img src="/assets/portfolio-1.png" alt="P1" className="worksImg" />
+        <img src={`${process.env.PUBLIC_URL}/assets/portfolio-1.png`} alt="P1" className="worksImg" />
         <img src="/assets/portfolio-2.png" alt="P2" className="worksImg" />
       </div>
 
